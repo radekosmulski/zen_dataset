73eb3340c250ce46c48e82b2d2ac88d577a19eeb
Radek Osmulski <rosmulski@gmail.com>
2019-02-12 18:43:32 +0100

    refactor

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,9 +2,10 @@ import pytest
 from zen_dataset.utils import *
 import tempfile
 
-def test_paths_to_files_in():
-    with tempfile.TemporaryDirectory() as tmpdir:
-        with tempfile.NamedTemporaryFile(dir=tmpdir):
-            with tempfile.TemporaryDirectory(dir=tmpdir) as tmpdir_inner:
-                with tempfile.NamedTemporaryFile(dir=tmpdir_inner):
-                    assert len(paths_to_files_in(tmpdir)) == 2
+class TestPathsToFilesIn:
+    def test_recursively_collects_file_paths_in_nested_directories(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            with tempfile.NamedTemporaryFile(dir=tmpdir):
+                with tempfile.TemporaryDirectory(dir=tmpdir) as tmpdir_inner:
+                    with tempfile.NamedTemporaryFile(dir=tmpdir_inner):
+                        assert len(paths_to_files_in(tmpdir)) == 2

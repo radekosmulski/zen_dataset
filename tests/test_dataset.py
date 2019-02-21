da34aae8635db66c39ed9cd640355f1c2875e1a7
Radek Osmulski <rosmulski@gmail.com>
2019-02-21 09:28:56 +0100

    decrease coupling between Dataset and readers / labelers

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,16 +1,14 @@
 import pytest
 from zen_dataset import Dataset
-from unittest.mock import Mock, MagicMock
+from unittest.mock import Mock, MagicMock, call
 
 @pytest.fixture
 def reader():
-    reader_instance = Mock(return_value='some data')
-    return Mock(return_value=reader_instance)
+    return Mock(return_value='some data')
 
 @pytest.fixture
 def labeler():
-    reader_instance = Mock(return_value='a target')
-    return Mock(return_value=reader_instance)
+    return Mock(return_value='a target')
 
 def test_is_imported():
     Dataset
@@ -19,13 +17,14 @@ def test_returns_len_of_items_in_dataset(reader, labeler):
     items = MagicMock()
     items.__len__.return_value = 10
     dataset = Dataset(items, reader, labeler)
-    assert len(dataset ) == 10
-    items.__len__.assert_called()
+    assert len(dataset) == 10
 
 def test_passes_item_to_reader_to_get_data(reader, labeler):
     dataset = Dataset([1,2,3], reader, labeler)
     assert dataset[1][0] == 'some data'
+    assert reader.call_args == call(2)
 
 def test_passes_item_to_labeler_to_get_target(reader, labeler):
     dataset = Dataset([1,2,3], reader, labeler)
     assert dataset[1][1] == 'a target'
+    assert reader.call_args == call(2)
--- zen_dataset/dataset.py
+++ zen_dataset/dataset.py
@@ -1,7 +1,6 @@
 class Dataset():
     def __init__(self, items, reader, labeler):
-        self.items = items
-        self._initialize(reader, labeler)
+        self.items, self.reader, self.labeler = items, reader, labeler
 
     def __len__(self):
         return len(self.items)
@@ -9,7 +8,3 @@ class Dataset():
     def __getitem__(self, idx):
         item = self.items[idx]
         return self.reader(item), self.labeler(item)
-
-    def _initialize(self, reader, labeler):
-        self.reader = reader(self.items)
-        self.labeler = labeler(self.items)

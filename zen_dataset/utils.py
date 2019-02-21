73c32a40676f614b9ad8785b47774a800045c7b8
Radek Osmulski <rosmulski@gmail.com>
2019-02-21 09:29:45 +0100

    add opening images, normalization

--- zen_dataset/utils.py
+++ zen_dataset/utils.py
@@ -1,4 +1,8 @@
+import torch
+import matplotlib.pyplot as plt
 from pathlib import Path
+import numpy as np
+import PIL
 
 def paths_to_files_in(dir):
     paths = []
@@ -8,3 +12,26 @@ def paths_to_files_in(dir):
         else:
             paths.append(path)
     return paths
+
+def open_image(path): return PIL.Image.open(path).convert('RGB')
+
+def image2tensor(image):
+    ary = np.asarray(image)
+    ary = ary.transpose(2, 0, 1)
+    tensor =  torch.from_numpy(ary.astype(np.float32, copy=False))
+    return tensor.div_(255)
+
+imagenet_stats = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+mean, std = torch.from_numpy(np.array(imagenet_stats).astype(np.float32))
+
+def imagenet_normalize(tensor):
+    zero_centered = tensor - mean[:, None, None]
+    return zero_centered / std[:, None, None]
+
+def imagenet_denormalize(zero_centered):
+    zero_centered = zero_centered * std[:, None, None]
+    return zero_centered + mean[:, None, None]
+
+def plot(tensor):
+    ary = tensor.numpy()
+    plt.imshow(ary.transpose(1,2,0))
